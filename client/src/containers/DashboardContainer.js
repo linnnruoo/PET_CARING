30bcc89b931b9e6d9d8ae8c4fe12899cb74b2de8
linnnruoo <linruo2014@gmail.com>
2019-04-04 14:49:05 +0800

    add dashboard container

---
+++ client/src/containers/DashboardContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class DashboardContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <></>;
+  }
+}
+
+export default DashboardContainer;
