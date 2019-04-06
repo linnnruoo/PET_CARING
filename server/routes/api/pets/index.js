bf842f2d64c200d84bd7b1ea389b06e322840b1f
linnnruoo <linruo2014@gmail.com>
2019-04-07 01:30:39 +0800

    add api to get pets

--- server/routes/api/pets/index.js
+++ server/routes/api/pets/index.js
@@ -1,36 +1,47 @@
-/* /api/pets/{x} */	
+/* /api/pets/{x} */
 
-const express = require('express');	
-const passport = require('passport');	
-const PetModel = require('../../../models/Pet');
-const router = express.Router();	
+const express = require("express");
+const passport = require("passport");
+const PetModel = require("../../../models/Pet");
+const router = express.Router();
 
+/**
+ * @route GET /api/pets/
+ * @desc: Get all the pets
+ * @access Public
+ */
+router.get("/", async (req, res) => {
+  PetModel.findAll()
+    .then(pets => res.json(pets))
+    .catch(err => res.status(500).json("Failed to retrieve all pets"));
+});
 
- /**	
- * @route POST /api/pets/add	
+/**
+ * @route POST /api/pets/add
  * @desc: To let pet owners add their pets
  * @access Private to pet owner
- */	
-router.post('/add', passport.authenticate('jwt', { session: false }),	
-    async (req, res) => {	
-      
-      const { name, breedName, age, gender} = req.body;	
+ */
+router.post(
+  "/create",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { name, ownerId, typeName, breedName, age, gender } = req.body;
+    PetModel.create(name, ownerId, typeName, breedName, age, gender)
+      .then(pet => res.json(pet))
+      .catch(err => res.status(500).json("Failed to add a new pet!"));
+  }
+);
 
-        try {	
-          const pet = await PetModel.create(name, id, breedName, age, gender);	
-          console.log(message);	
-        } catch (error) {	
-          console.log(error);	
-        
-        res.json({	
-            me: {	
-              ...req.body,	
-              message: 'Pet added!',	
-            }	
-          });	
-      
-       }    	
-    },	
-  );	
+/**
+ * @route GET /api/pets/add
+ * @desc: Get all the pets under this owner
+ * @access Private to pet owner
+ */
+router.get("/:ownerId", async (req, res) => {
+  const ownerId = req.params.ownerId;
+  PetModel.filterByOwner(ownerId)
+    .then(pets => res.json(pets))
+    .catch(err => res.status(404).json("No pets found under this user."));
+});
 
- module.exports = router; 
+module.exports = router;
