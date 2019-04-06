3f782934a08aa3db10447ebd459aedb1b0a90d39
QzSG <quantumparadoxes@gmail.com>
2019-04-06 20:10:59 +0800

    Fix tables

--- server/ddl/bids.sql
+++ server/ddl/bids.sql
@@ -2,13 +2,13 @@ drop table if exists bids cascade;
 
 create table bids
 (
-  id				serial							not null,
-  sid				serial							not null,
+  id				int							not null,
+  sid				int							not null,
   amount			int								not null,
   petName			varchar(100)					not null,
   accepted			boolean							default FALSE,
   primary key (id, sid, petName),
   foreign key (id) references owners,
   foreign key (sid) references services,
-  foreign key (petName) references pets 
+  foreign key (petName, id) references pets 
 );
