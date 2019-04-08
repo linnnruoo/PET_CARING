632e1b33b49c14bdf5d865a2619c00bdbb3664ba
linnnruoo <linruo2014@gmail.com>
2019-04-08 23:22:10 +0800

    fix syntax

--- server/models/Service.js
+++ server/models/Service.js
@@ -20,8 +20,12 @@ const Service = {
   },
 
   getOne: async sid => {
-    const getOneQuery = `SELECT u.id, u.first_name, u.last_name, s.sid, s.title, s.startTime, s.endTime, s.typeName, s.expected
-        FROM services s JOIN users u on u.id = s.id AND s.id = $1`;
+    const getOneQuery = `
+        SELECT u.id, u.first_name, u.last_name,
+            s.sid, s.title, s.startTime, s.endTime, s.typeName, s.expected
+        FROM services s JOIN users u
+        ON u.id = s.id AND s.sid = $1`;
+
     const values = [sid];
     try {
       const { rows } = await db.query(getOneQuery, values);
