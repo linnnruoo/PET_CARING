930604b692eaa947f5d5eba57807933cea553a74
jieliangang <jieliangang@gmail.com>
2019-04-08 18:20:47 +0800

    Pet cannot be in two accepted bid at any time

--- server/trigger/trigger_bid.sql
+++ server/trigger/trigger_bid.sql
@@ -1,6 +1,7 @@
 CREATE OR REPLACE FUNCTION bid_check()
 RETURNS TRIGGER AS $$
 DECLARE count NUMERIC;
+DECLARE count2 NUMERIC;
 BEGIN
 	SELECT COUNT(*) INTO count
 	FROM services s
@@ -12,6 +13,13 @@ BEGIN
 		AND p.name = NEW.petName
 		AND p.typeName = s.typeName
 	);
+	SELECT COUNT(*) INTO count2
+	FROM bids b inner join services s on b.sid = s.sid
+	WHERE b.id = NEW.id 
+	AND b.petName = NEW.petName
+	AND s.startTime <= NEW.endTime
+	AND NEW.startTime <= s.endTime
+	AND b.accepted = true;
 	-- Check if bid is modified after accepted
 	IF TG_OP = 'UPDATE' AND OLD.accepted = true AND (OLD.amount <> NEW.amount OR OLD.petName <> NEW.petName)
 		RAISE NOTICE 'You can not update your bid after it is accepted!';
@@ -19,6 +27,10 @@ BEGIN
 	-- Check if pet type matches service pet type
 	ELSIF count = 0 THEN
 		RAISE NOTICE 'Pet type does not match the service!';
+		RETURN NULL;
+	-- Check if pet is already in another accepted service 
+	ELSIF count2 = 0 THEN
+		RAISE NOTICE 'Pet is in another accepted service!';
 		RETURN NULL;
 	-- Check if bid amount is not negative or null
 	ELSIF NEW.amount < 0 OR NEW.amount IS NULL THEN
